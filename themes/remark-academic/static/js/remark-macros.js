cd007eded0f404f91cf3b2c29b8cf9f5c71bca63
Jason L Causey <11724284+jcausey-astate@users.noreply.github.com>
2022-12-20 18:58:16 -0600

    added "efficient sorting" lecture

--- themes/remark-academic/static/js/remark-macros.js
+++ themes/remark-academic/static/js/remark-macros.js
@@ -5,3 +5,16 @@ remark.macros['img'] = function (altText, width) {
 };
 
 remark.macros['scale'] = remark.macros['img'];
+
+// remark.macros['width'] = function (altText, width) {
+//   var url = this;
+//   return '<img alt="' + altText + '" src="' + url + '" style="width: ' + width + '" />';
+// };
+// remark.macros['height'] = function (altText, height) {
+//   var url = this;
+//   return '<img alt="' + altText + '" src="' + url + '" style="height: ' + height + '" />';
+// };
+// remark.macros['top'] = function (altText, top) {
+//   var url = this;
+//   return '<img alt="' + altText + '" src="' + url + '" style="top: ' + top + '" />';
+// };
